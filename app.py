9b57495dd88159d9268b2f24ac58c43f1900ad66
deep9893 <deeppratapsingh0996@gmail.com>
2023-06-15 17:49:19 +0530

    main.py & app.py file creted

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template,request
+import re
+from nltk.stem import WordNetLemmatizer
+import pickle
+
+wo = WordNetLemmatizer()
+
+app = Flask(__name__)
+
+def preprocess(data):
+    #preprocess
+    a = re.sub('[^a-zA-Z]',' ',data)
+    a = a.lower()
+    a = a.split()
+    a = [wo.lemmatize(word) for word in a ]
+    a = ' '.join(a)
+    return a
+
+
+tfidf_vectorizer = pickle.load(open('vectorizer.pkl','rb'))
+model =  pickle.load(open('prediction.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods= ['POST'])
+def predict():
+    msg = request.form['mood_pred']
+    a = preprocess(msg)
+
+    result = model.predict(tfidf_vectorizer.transform([a]))[0]
+
+    return render_template('index.html',pred = "You are--{}".format(result))
+    
+    # pred = model.predict(tfidf_vectorizer.transform([a]))[0]
+
+    # result = pred
+
+    # if result == 0:
+    #     return render_template('index.html', final_result="Not Depressed")
+
+    # elif result == 1:
+    #     return render_template('index.html', final_result="Depressed")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
